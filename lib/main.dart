9862f688467f57a26a42894df6c682df7d35ad5b
triankyy <triankyy@gmail.com>
2023-08-08 19:11:28 +0700

    fix: dateformat localization

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,21 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 import 'package:tambak_undang/services/report_service.dart';
 import 'package:tambak_undang/services/sharedpref.dart';
 import 'package:tambak_undang/splash/splash.dart';
 
 final today = DateUtils.dateOnly(DateTime.now());
 
-void main() {
+void main() async {
+  Intl.defaultLocale = "id_ID";
   WidgetsFlutterBinding.ensureInitialized();
   SharedPref.init().then((value) {
     Timer.periodic(const Duration(hours: 2), (Timer t) => getCurrentReport().then((value) => value.fold((l) => SharedPref.saveCurrentReport(l), (r) => null)));
   });
   WidgetsFlutterBinding.ensureInitialized();
-  initializeDateFormatting('id_ID');
+  await initializeDateFormatting('id_ID');
   initializeDateSymbol();
   runApp(const MyApp());
 }
