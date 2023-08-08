adbe7da46f9aa3ea2e5b38251a6f1676e0beded0
triankyy <triankyy@gmail.com>
2023-08-08 16:39:42 +0700

    refactor: remove late variable

--- lib/services/sharedpref.dart
+++ lib/services/sharedpref.dart
@@ -2,7 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tambak_undang/models/report_model.dart';
 
 class SharedPref {
-  static late final SharedPreferences preferences;
+  static SharedPreferences? preferences;
   static bool _init = false;
   static Future init() async {
     if (_init) return;
@@ -11,13 +11,13 @@ class SharedPref {
     return preferences;
   }
 
-  static void saveUsername(String username) => preferences.setString("username", username);
-  static String? get getUsername => preferences.getString("username");
-  static void saveToken(String token) => preferences.setString("token", token);
-  static String? get getToken => preferences.getString("token");
+  static void saveUsername(String username) => preferences?.setString("username", username);
+  static String? get getUsername => preferences?.getString("username");
+  static void saveToken(String token) => preferences?.setString("token", token);
+  static String? get getToken => preferences?.getString("token");
 
-  static void saveCurrentReport(ReportModel report) => preferences.setString("currentReport", reportModelToJson(report));
-  static ReportModel? get getCurrentReport => reportModelFromJson(preferences.getString("currentReport") ?? "");
+  static void saveCurrentReport(ReportModel report) => preferences?.setString("currentReport", reportModelToJson(report));
+  static ReportModel? get getCurrentReport => reportModelFromJson(preferences?.getString("currentReport") ?? "");
 
-  static void logout() => preferences.clear();
+  static void logout() => preferences?.clear();
 }
