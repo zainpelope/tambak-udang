d15ec8e5bb0240017850c8d252b0657e33f81ec6
triankyy <triankyy@gmail.com>
2023-08-08 19:12:59 +0700

    feat: get data from API

--- lib/services/report_service.dart
+++ lib/services/report_service.dart
@@ -18,3 +18,33 @@ Future<Either<ReportModel, String>> getCurrentReport() async {
     return const Right("Terjadi kesalahan saat mengambil data laporan terbaru");
   }
 }
+
+Future<Either<List<ReportModel>, String>> getAllReport() async {
+  try {
+    final Response response = await http.get(
+      "/report/all", 
+      options: Options(headers: {
+        "Authorization": "Bearer ${SharedPref.getToken}"
+      }));
+    if (response.statusCode == 200) return Left((response.data["data"] as List).map((e) => ReportModel.fromJson(e)).toList());
+    return Right(response.data["message"] ?? "Tidak ada data");
+  } on DioException catch (e) {
+    print(e);
+    return const Right("Terjadi kesalahan saat mengambil data laporan");
+  }
+}
+
+Future<Either<List<ReportModel>, String>> getReportByRange({required String from, required String to}) async {
+  try {
+    final Response response = await http.get(
+      "/report/range?from=$from&to=$to", 
+      options: Options(headers: {
+        "Authorization": "Bearer ${SharedPref.getToken}"
+      }));
+    if (response.statusCode == 200) return Left((response.data["data"] as List).map((e) => ReportModel.fromJson(e)).toList());
+    return Right(response.data["message"] ?? "Tidak ada data");
+  } on DioException catch (e) {
+    print(e);
+    return const Right("Terjadi kesalahan saat mengambil data laporan");
+  }
+}
